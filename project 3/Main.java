95d656b466dc5967162700f781f7f72dd8a73a9e
srajan14 <shubhdubeyy702@gmail.com>
2024-09-12 20:13:33 +0530

    Add files via upload

---
+++ project 3/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args) {
+        for(int a=15;a>1;a--){
+            System.out.println("the value of a is "+a); 
+            
+        }
+    }
+}
